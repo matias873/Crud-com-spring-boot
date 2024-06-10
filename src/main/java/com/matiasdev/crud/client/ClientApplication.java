c7748947ef2a327f804f79506b506b15191210b1
matiasdev <matiasdev873@gmail.com>
2024-06-10 17:49:43 -0300

    frist commit

---
+++ src/main/java/com/matiasdev/crud/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.matiasdev.crud.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matiasdev/crud/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasdev.crud.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
