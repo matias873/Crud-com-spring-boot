575c6b6e1b6e820721f6d83f064ec1419bb0ff1a
matiasdev <matiasdev873@gmail.com>
2024-06-18 10:59:55 -0300

    Add bean validation -Clients Dto validation

--- src/main/java/com/matiasdev/crud/client/dto/ClientsDto.java
+++ src/main/java/com/matiasdev/crud/client/dto/ClientsDto.java
@@ -2,6 +2,8 @@ package com.matiasdev.crud.client.dto;
 
 
 import com.matiasdev.crud.client.entities.Client;
+import jakarta.validation.constraints.*;
+
 
 import java.time.LocalDate;
 
@@ -9,10 +11,22 @@ public class ClientsDto {
 
 
     private Long id;
+    @NotBlank(message = "O nome não pode ser vazio")
+    @Size(min = 2, max = 50, message = "O nome deve ter entre 2 e 50 caracteres")
     private  String nome ;
+
+    @NotBlank(message = "O CPF não pode ser vazio")
+    @Pattern(regexp = "\\d{11}", message = "O CPF deve ter 11 dígitos")
     private  String cpf;
+
+    @NotNull(message = "O salário não pode ser nulo")
+    @Positive(message = "O salário deve ser positivo")
     private Double salary;
+
+    @NotNull(message = "A data de nascimento não pode ser nula")
+    @PastOrPresent(message = "A data de nascimento deve ser no passado")
     private LocalDate birthDate;
+
 
     public ClientsDto(Long id, String nome, String cpf, Double salary, LocalDate birthDate) {
         this.id = id;
